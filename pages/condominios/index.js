449e43c279861e3052e56d39a3b5ee057fa31e80
“Elias” <eliaskiso4@gmail.com>
2023-06-17 00:13:36 -0300

    alterando 'cards' e adiconando biblioteca MUI

--- pages/condominios/index.js
+++ pages/condominios/index.js
@@ -1,65 +1,84 @@
 import Pagina from '@/components/Pagina'
 import React from 'react'
-import { Button, Card, Col, Row } from 'react-bootstrap'
+import { Col, Row } from 'react-bootstrap'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const index = () => {
     return (
         <Pagina>
-            <Row md={4}>
+            <Row md={3}>
                 <Col>
-                    <Card border='success' style={{ width: '16rem' }}>
-                        <Card.Img variant="top" src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alphavillebrasilia.net.br%2Falphaville-brasilia-df%2F&psig=AOvVaw0xUsxJ5hmHyi9-YAIoN3Ht&ust=1687048892596000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMjElo2Jyf8CFQAAAAAdAAAAABAI' />
-                        <Card.Body>
-                            <Card.Title>Condomínio Alphaville</Card.Title>
-                            <Card.Text>
+                    <Card >
+                        <CardMedia
+                            component="img"
+                            height="250"
+                            image='../images/alphaville.jpg'
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Alphaville
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
                                 Ocupando uma área de mais de 20 milhões de metros quadrados de pura qualidade de vida, a região foi escolhida e
                                 cuidadosamente pensada para abrigar um projeto urbano sofisticado e totalmente voltado para o bem estar,
                                 conveniência, segurança e lazer de seus moradores.
-                            </Card.Text>
-                            <Button variant="success">Alugar</Button>
-                        </Card.Body>
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button >Alugar</Button>
+                        </CardActions>
                     </Card>
                 </Col>
 
                 <Col>
-                    <Card border='success' style={{ width: '16rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Condomínio</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="success">Alugar</Button>
-                        </Card.Body>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="250"
+                            image=""
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Living Park Sul
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button >Alugar</Button>
+                        </CardActions>
                     </Card>
                 </Col>
+
                 <Col>
-                    <Card border='success' style={{ width: '16rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Condomínio</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="success">Alugar</Button>
-                        </Card.Body>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="250"
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button>Alugar</Button>
+                        </CardActions>
                     </Card>
                 </Col>
-                <Col>
-                    <Card border='success' style={{ width: '16rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-                        <Card.Body>
-                            <Card.Title>Condomínio</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="success">Alugar</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
+
             </Row>
         </Pagina>
     )
